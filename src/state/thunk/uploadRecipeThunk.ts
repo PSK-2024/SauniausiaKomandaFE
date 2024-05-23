95af04ac200cc42bc7e17162ea21b3aaf20e43d4
TadasSabestinas <tadassabestinas@gmail.com>
2024-05-23 20:12:35 +0300

    Adding interceptor for uploading recipe too

--- src/state/thunk/uploadRecipeThunk.ts
+++ src/state/thunk/uploadRecipeThunk.ts
@@ -2,6 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { RecipeUploadData } from '../model/uploadRecipeModel';
 import { BASE_URL, PATHS } from '../../api/paths';
+import api from '../../api/api';
 
 export const uploadRecipe = createAsyncThunk<
   RecipeUploadData,
@@ -9,7 +10,7 @@ export const uploadRecipe = createAsyncThunk<
   { rejectValue: string }
 >('uploadRecipe/uploadRecipe', async (recipeData, { rejectWithValue }) => {
   try {
-    const response = await axios.post<RecipeUploadData>(
+    const response = await api.post<RecipeUploadData>(
       `${BASE_URL}${PATHS.UPLOAD_RECIPE_PATH}`,
       recipeData
     );
